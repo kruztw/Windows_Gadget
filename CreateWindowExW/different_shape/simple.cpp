7e5a204b03eed345e9be16ef93dc1b85f68c4b5e
raybai <raybai@synology.com>
2023-09-08 11:38:58 +0800

    add createWindowExW/different_shape

---
+++ CreateWindowExW/different_shape/simple.cpp
@@ -0,0 +1,96 @@
+#include <Windows.h>
+#include <tchar.h>
+
+int main()
+{
+    static TCHAR applicationName[] = _T("DesktopApp");
+    static TCHAR title[] = _T("title");
+    LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = GetModuleHandle(NULL);
+    wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = applicationName;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+    {
+        MessageBox(NULL,
+            _T("Call to RegisterClassEx failed!"),
+            _T("Windows Desktop Guided Tour"),
+            NULL);
+
+        return 1;
+    }
+
+    HWND hWnd = CreateWindowEx(
+        WS_EX_TOPMOST,
+        applicationName,
+        title,
+        WS_DLGFRAME, // the type of window to create
+        0, 0, // (x, y)
+        500, 500, // (width, length)
+        NULL, // the parent of this window
+        NULL, // this application does not have a menu bar
+        wcex.hInstance,
+        NULL // not used in this application
+    );
+
+    if (!hWnd)
+    {
+        MessageBox(NULL,
+            _T("Call to CreateWindow failed!"),
+            _T("Windows Desktop Guided Tour"),
+            NULL);
+
+        return 1;
+    }
+
+    HRGN hRgnWnd = CreateEllipticRgn(0, 0, 100, 100); // create a elliptic region
+    SetWindowRgn(hWnd, hRgnWnd, TRUE); // restrict the window(hWnd) in the elliptic region (hRgnWnd)
+
+    ShowWindow(hWnd, SW_SHOW);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    TCHAR greeting[] = _T("Hello, Windows desktop!");
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        TextOut(hdc, 5, 10, greeting, _tcslen(greeting));
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
